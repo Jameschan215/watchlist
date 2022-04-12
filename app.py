b42e7952ddab7faeef55a9eff08425826a05eba6
James Chen <chenjames215@gmail.com>
2022-04-12 11:34:51 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-from flask import Flask, escape, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+name = "James Chen"
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
-@app.route("/user/<name>")
-def user_page(name):
-    return "User: {}".format(escape(name))
 
-@app.route("/test")
-def test_url_for():
-    print(url_for("hello"))
-
-    print(url_for("user_page", name="james"))
-
-    print(url_for("user_page", name="frank"))
-
-    print(url_for("test_url_for"))
-
-    print(url_for("test_url_for", num=2))
-
-    return "Test page"
+@app.route("/")
+def index():
+    return render_template("index.html", name=name, movies=movies)
